69613e0067a860f83a1f5a07a18184024c355be6
fikri <fikribakhtiar26@gmail.com>
2023-09-21 01:34:40 +0700

    add Webpack Merge

---
+++ webpack.common.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  //Entry
+  entry: "./src/index.js",
+
+  //Output
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  //Module
+  module: {
+    rules: [
+      //CSS Style and CSS Loader
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+      //Babel Loader
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  //Plugin
+  plugins: [
+    //HtmlWebpackPlugin
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "index.html",
+    }),
+  ],
+};
