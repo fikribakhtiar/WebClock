94ea84e9740673ac6031adac9cb1698aeceec584
fikri <fikribakhtiar26@gmail.com>
2023-09-21 00:52:48 +0700

    add Babel Loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,18 @@ module.exports = {
                         loader: 'css-loader'
                     }
                 ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:[
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets:['@babel/preset-env']
+                        }
+                    }
+                ]
             }
         ]
     }
